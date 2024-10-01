c88d794e3ee7900646de741042323c738f1c7f9a
Christian Lin <christianlinlin123@gmail.com>
2024-10-01 12:21:21 -0400

    added media queries

--- src/js/about.js
+++ src/js/about.js
@@ -1,1 +1,12 @@
 import "../sass/about.scss";
+
+document.addEventListener("DOMContentLoaded", function() {
+
+    const hamburgerMenu = document.querySelector('.hamburger');
+    const sideBar = document.querySelector('.responsive-sidebar');
+
+    hamburgerMenu.addEventListener('click', () => {
+        sideBar.classList.toggle('responsive-sidebar-toggle');
+        hamburgerMenu.classList.toggle('hamburger-animation');
+    });
+});
